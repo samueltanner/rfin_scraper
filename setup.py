b548bfe9edd4eb4cbbb503214280b785804b44e5
Sam Tanner <samuel.m.s.tanner@gmail.com>
2023-05-12 11:18:27 -0700

    phew... finally updates to v.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='finny_scraper',
-    version='0.8',
+    version='0.9',
     description='A package for scraping property information from Finny website',
     long_description=long_description,
     long_description_content_type='text/markdown',
